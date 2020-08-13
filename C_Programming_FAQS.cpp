add75414015ffb1b3d93500295b808c2ff37378b
luopan <709165253@qq.com>
2020-08-13 10:17:21 +0800

    计算C++ 类的大小测试

--- C_Programming_FAQS.cpp
+++ C_Programming_FAQS.cpp
@@ -1,11 +1,124 @@
-﻿// C_Programming_FAQS.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// C_Programming_FAQS.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
 
+// 第一章：关于对象
+
+// 声明一个struct Point3d - C语言风格
+typedef struct point3d
+{
+	float x;
+	float y;
+	float z;
+} Point3d;
+
+// 打印Point3d的函数 - C语言风格
+void Point3d_print(Point3d *pPoint3d)
+{
+	printf("(%g, %g, %g)", pPoint3d->x, pPoint3d->y, pPoint3d->z);
+}
+
+// 打印Point3d的宏 - C语言风格
+#define Point3d_print(point) \
+   printf("(%g, %g, %g)", point->x, point->y, point->z);
+
+
+// 抽象数据类型 （ADT，abstract data type） - C++语言风格
+class Point3D
+{
+public:
+	Point3D(float x = 0.0f, float y = 0.0f, float z = 0.0f)
+		: _x(x), _y(y), _z(z) {}
+
+	float GetX() 
+	{
+		return _x;
+	}
+	
+	float GetY()
+	{
+		return _y;
+	}	
+	
+	float GetZ()
+	{
+		return _z;
+	}
+
+	void SetX(float tmp) 
+	{
+		_x = tmp;
+	}
+
+	void SetY(float tmp)
+	{
+		_y = tmp;
+	}
+
+	void SetZ(float tmp)
+	{
+		_z = tmp;
+	}
+
+private:
+	float _x;
+	float _y;
+	float _z;
+};
+
+// C++ 类的大小计算
+class A {};
+
+class B 
+{
+public:
+	B() = default;
+	~B() = default;
+private:
+	static int a;
+	int b;
+	char c;
+};
+
+class C : public A 
+{
+	int a;
+};
+
+class D {
+	virtual void f() { std::cout << "D::f" << std::endl; }
+
+	virtual void g() { std::cout << "D::g" << std::endl; }
+
+	virtual void h() { std::cout << "D::h" << std::endl; }
+};
+
+class E {
+	int a;
+
+	virtual void f() { std::cout << "D::f" << std::endl; }
+
+	virtual void g() { std::cout << "D::g" << std::endl; }
+
+	virtual void h() { std::cout << "D::h" << std::endl; }
+};
+
+void test_class_size()
+{
+	// 64位机器
+	std::cout << "The A size without any memeber is: "                     << sizeof(A) << std::endl;	// 1
+	std::cout << "The B size with normal and static member is: "           << sizeof(B) << std::endl;	// 8
+	std::cout << "The C size with extends A and add normal member is: "    << sizeof(C) << std::endl;	// 4
+	std::cout << "The D size with virtual function is: "                   << sizeof(D) << std::endl;	// 8
+	std::cout << "The E size with virtual function and normal member is: " << sizeof(E) << std::endl;	// 16
+};
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	test_class_size();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
