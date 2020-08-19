99ff1288530248c8e3a69a8381203328750f827c
luopan <709165253@qq.com>
2020-08-19 14:35:21 +0800

    仿函数

--- C_Programming_FAQS.cpp
+++ C_Programming_FAQS.cpp
@@ -326,12 +326,95 @@ void test_map() {
 
 }
 
+// 仿函数：本质就是类重载了operator(),创建一个具备函数能力的对象
+#include <algorithm>
+
+// C++ 原始函数
+bool MySort(int a, int b) {
+	return a < b;
+}
+
+void Display(int a) {
+	cout << a << " ";
+}
+
+// C++模板函数
+template<class T>
+inline bool MySortT(T const& a, T const& b) {
+	return a < b;
+}
+
+template<class T>
+inline void DisplayT(T const& a) {
+	cout << a << " ";
+}
+
+// C++ 仿函数
+struct SortF 
+{
+	bool operator() (int a, int b) {
+		return a < b;
+	}
+};
+
+struct DisplayF
+{
+	void operator() (int a) {
+		cout << a << " ";
+	}
+};
+
+// C++仿函数模板
+template<class T>
+struct SortTF
+{
+	inline bool operator() (T const& a, T const& b) const {
+		return a < b;
+	}
+};
+
+template<class T>
+struct DisplayTF
+{
+	inline void operator() (T const& a) const {
+		cout << a << " ";
+	}
+};
+
+void test_imitate_function() {
+	// C++方式
+	int arr[] = { 4, 3, 2, 1, 7 };
+	sort(arr, arr + 5, MySort);
+	for_each(arr, arr + 5, Display);
+	cout << endl;
+
+	// C++泛型
+	int arr2[] = { 4, 3, 2, 1, 7 };
+	sort(arr2, arr2 + 5, MySortT<int>);
+	for_each(arr2, arr2 + 5, DisplayT<int>);
+	cout << endl;
+
+	// C++仿函数
+	int arr3[] = { 4, 3, 2, 1, 7 };
+	sort(arr3, arr3 + 5, SortTF<int>());
+	for_each(arr3, arr3 + 5, DisplayTF<int>());
+	cout << endl;
+
+	// C++仿函数模板
+	int arr4[] = { 4, 3, 2, 1, 7 };
+	sort(arr4, arr4 + 5, SortF());
+	for_each(arr4, arr4 + 5, DisplayF());
+	cout << endl;
+}
+
+
 int main()
 {
 	test_class_size();
 	test_template_sum();
 	test_STL();
 	test_map();
+	test_imitate_function();
 	return 0;
 }
 
